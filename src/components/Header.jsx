6b086f2c8e2eb196fbea695414aa9eb882d39e65
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-02-14 13:42:12 -0800

    add banner pic, working on placement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import { Router } from 'react-router-dom';
-
+import bannerPic from './bannerPic.jpg'
 function Header(){
+
+  const headerstyle = {
+    textAlign: 'center',
+  }
+
+  const banner = {
+    position: "absolute",
+    left: "0px",
+    top: "0px",
+    zIndex: "-1",
+    width: "100vw",
+  }
   return (
-    <div>
-    <Link to="/">Home</Link> | <Link to="/newkeg">Create New Keg</Link>
-    <h1> The Taproom. </h1>
-    <hr/>
+    <div style={headerstyle}>
+      <div style={banner}>
+        <h1> The Vanguard Taproom </h1>
+        <Link to="/">Home</Link> | <Link to="/newkeg">Create New Keg</Link>
+        <img src={bannerPic}/>
+      </div>
+
     </div>
   );
 }
