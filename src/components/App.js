0af5e2758cfd9c9cbad2db5ef9e55f37c3c4e032
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-02-21 08:29:31 -0800

    add state to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import './App.css';
 import Header from "./Header";
@@ -7,27 +8,36 @@ import { Switch, Route } from 'react-router-dom';
 import Error404 from './Error404';
 import Footer from './Footer';
 
-function App(){
-  var allStyle = {
-    backgroundColor: 'rgb(48,48,48)',
-    color: '#f8f8f8',
-    textAlign: 'left',
-    margin: '-8px',
-    textShadow: '5px 3px 5px black'
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterKegList: []
+    };    
   }
-  return (
-    <div style={allStyle}>
-    <Header/>
 
-    <Switch>
-    <Route exact path='/' component={KegList} />
-    <Route path='/newkeg' component={NewKegForm} />
-    <Route component={Error404} />
-    </Switch>
+  render() {
+    var allStyle = {
+      backgroundColor: 'rgb(48,48,48)',
+      color: '#f8f8f8',
+      textAlign: 'left',
+      margin: '-8px',
+      textShadow: '5px 3px 5px black'
+    }
+    return (
+      <div style={allStyle}>
+        <Header/>
+
+        <Switch>
+          <Route exact path='/' component={KegList} />
+          <Route path='/newkeg' component={NewKegForm} />
+          <Route component={Error404} />
+        </Switch>
         <Footer/>
-
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
